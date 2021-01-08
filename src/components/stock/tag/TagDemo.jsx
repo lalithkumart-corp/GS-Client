02103dec55bfed4ce78e166c6920749197298b44
Lalith <lalithkmr94@gmail.com>
2021-01-08 14:10:05 +0530

    tag layout 2

--- src/components/stock/tag/TagDemo.jsx
+++ src/components/stock/tag/TagDemo.jsx
@@ -7,10 +7,13 @@ export default class TagDemo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            leftContent: "wt: 0.00gm",
+            wtContent: "0.000",
             storeSuffix: "",
             rightContent: "wt: 0.00",
-            meltingTouch: 75
+            meltingTouch: 75,
+            makingCharge: 0,
+            manufacturer: '',
+            wastage: 0
         }
         this.inputControls.onChange = this.inputControls.onChange.bind(this);
     }
@@ -18,14 +21,23 @@ export default class TagDemo extends Component {
         onChange: (e, val, identifier) => {
             let newState = {...this.state};
             switch(identifier) {
-                case 'leftContent':
-                    newState.leftContent = val;
+                case 'wtContent':
+                    newState.wtContent = val;
                     break;
                 case 'rightContent':
                     newState.rightContent = val;
                     break;
                 case 'meltingTouch':
                     newState.meltingTouch = val;
+                    break;
+                case 'makingCharge':
+                    newState.makingCharge = val;
+                    break;
+                case 'manufacturer': 
+                    newState.manufacturer = val;
+                    break;
+                case 'wastage':
+                    newState.wastage = val;
                     break;
             }
             this.setState(newState);
@@ -37,24 +49,27 @@ export default class TagDemo extends Component {
                 <Row className="gs-card">
                     <Col className="gs-card-content">
                         <textarea 
-                            value={this.state.leftContent}
-                            onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'leftContent') }
+                            value={this.state.wtContent}
+                            onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'wtContent') }
                             />
                         {/* <input 
                             type="text" 
-                            value={this.state.leftContent}
-                            onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'leftContent') }
+                            value={this.state.wtContent}
+                            onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'wtContent') }
                             /> */}
-                        <textarea 
+                        {/* <textarea 
                             value={this.state.rightContent}
                             onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'rightContent') }
-                            />
+                            /> */}
                         {/* <input 
                             type="text" 
                             value={this.state.weight}
                             onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'weight') }
                             /> */}
-                        <input type="text" value={this.state.meltingTouch} onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'meltingTouch')} />
+                        {/* <input type="text" value={this.state.meltingTouch} onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'meltingTouch')} /> */}
+                        MC: <input type="text" value={this.state.makingCharge} onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'makingCharge')} />
+                        Wastage: <input type="text" value={this.state.wastage} onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'wastage')} />
+                        CODE:<input type="text" value={this.state.manufacturer} onChange={ (e) => this.inputControls.onChange(null, e.target.value, 'manufacturer')} />
                         <ReactToPrint
                             trigger={() => {
                                 // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
@@ -67,7 +82,7 @@ export default class TagDemo extends Component {
                 </Row>
                 <Row className="gs-card">
                     <Col className="gs-card-content">
-                        <Tag ref={el => (this.componentRef = el)} leftContent={this.state.leftContent} rightContent={this.state.rightContent} meltingTouch={this.state.meltingTouch} />
+                        <Tag ref={el => (this.componentRef = el)} wtContent={this.state.wtContent} rightContent={this.state.rightContent} meltingTouch={this.state.meltingTouch} makingCharge={this.state.makingCharge} manufacturer={this.state.manufacturer} wastage={this.state.wastage}/>
                     </Col>
                 </Row>
             </Container>
@@ -81,16 +96,19 @@ class Tag extends Component {
     }
     render() {
         return (
-            <div>
-                <div style={{width: "280px", height: "50px", backgroundColor: "#ecebeb", display: "inline-block", verticalAlign: "middle", fontSize: '17px'}}>
+            <div style={{fontFamily: 'initial'}}>
+                <div style={{width: "295px", height: "65px", backgroundColor: "#ecebeb", display: "inline-block", verticalAlign: "middle", fontSize: '17px', paddingTop: '2px'}}>
                     <Row>
-                        <Col xs={6} className="left-side" style={{ display: "inline-block", verticalAlign: "middle", whiteSpace: "pre-wrap", paddingRight: 0, paddingTop: '0', paddingLeft: '32px'}}>
-                            <div style={{textAlign: "center"}}>MJK</div>
-                            <div style={{textAlign: "center", fontSize: '20px'}}>{this.props.leftContent}</div>
+                        <Col xs={6} className="left-side" style={{ display: "inline-block", verticalAlign: "middle", whiteSpace: "pre-wrap", paddingRight: 0, paddingTop: '5px', paddingLeft: '59px'}}>
+                            <div style={{textAlign: "left", paddingTop: '13px', lineHeight: '8px'}}>MJK{this.props.manufacturer}</div>
+                            <div style={{textAlign: "left", fontSize: '22px', fontWeight: 'bold', paddingTop: '4px'}}><span style={{fontFamily: 'sans-serif'}}>wt</span> <span style={{fontFamily: 'sans-serif'}}>{this.props.wtContent}</span></div>
                         </Col>
-                        <Col xs={6} className="right-side" style={{ display: "inline-block", verticalAlign: "middle", whiteSpace: "pre-wrap"}}>
-                            <div style={{textAlign: "center", paddingTop: '6px', fontSize: '22px'}}>{this.props.rightContent}</div>
-                            <div style={{fontSize: '14px', textAlign: 'right', paddingTop: '0', position: 'absolute', right: '21px', top: '34px'}}>{this.props.meltingTouch}</div>
+                        <Col xs={6} className="right-side" style={{ display: "inline-block", verticalAlign: "middle", whiteSpace: "pre-wrap", paddingLeft: '18px', paddingTop: '7px'}}>
+                            <div style={{textAlign: "left", paddingTop: '8px', fontSize: '22px'}}>
+                                <div style={{fontSize: '18px', fontWeight: 'normal', paddingLeft: '5px', lineHeight: '9px'}}>MC: {this.props.makingCharge}  {this.props.wastage}</div>
+                                <div style={{fontSize: '25px', fontWeight: 'bold', paddingTop: '3px'}}> <span style={{fontFamily: 'sans-serif'}}>wt</span> <span style={{fontFamily: 'sans-serif'}}>{this.props.wtContent}</span></div>
+                            </div>
+                            {/* <div style={{fontSize: '16px', textAlign: 'right', paddingTop: '0', position: 'absolute', right: '15px', top: '36px'}}>{this.props.meltingTouch}</div> */}
                         </Col>
                     </Row>
                 </div>
