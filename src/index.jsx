e1788e16793d16d49f78628a4b63baa1a16e8c50
lalithkumart <lalithkmr94@gmail.com>
2026-09-20 17:16:44 +0530

    registry worker changes

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-registerServiceWorker();
+// registerServiceWorker();
 
